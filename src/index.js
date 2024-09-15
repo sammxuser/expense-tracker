6b622628212bc88ee59ecc6e7260dc8c9abb0094
Samuel Mburu <sammxuser@gmail.com>
2024-09-15 22:30:48 +0300

    transaction history table, total expense and income,clearALL button

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const incomeAmount = document.getElementById('income-amount');
 const totalExpenses = document.getElementById('total-expenses');
 const totalIncome = document.getElementById('total-income');
 const balance = document.getElementById('balance');
+const transactionHistory = document.getElementById('transaction-history');
+
 addExpenseBtn.addEventListener('click', () => {
   if (
     expenseDescription.value !== '' ||
@@ -41,10 +43,11 @@ addIncomeBtn.addEventListener('click', () => {
     incomeAmount.value,
     'income'
   );
+  incomeAmount.value = '';
+  incomeDescription.value = '';
 });
 
 function addTransactions(description, category = '', amount, type) {
-  const transactionHistory = document.getElementById('transaction-history');
   const transRow = document.createElement('tr');
   const descColumn = document.createElement('td');
   descColumn.textContent = description;
@@ -64,13 +67,38 @@ function addTransactions(description, category = '', amount, type) {
   transRow.appendChild(actionColumn);
 
   transactionHistory.appendChild(transRow);
-  let runningBal = 0;
-  if (type === 'income') {
-    totalIncome.textContent = amount;
-    runningBal += amount;
-  } else if (type === 'expense') {
-    totalExpenses.textContent = amount;
-    runningBal -= amount;
+  // Calculate balance
+  let runningBal = 0.0;
+  let totIncome = 0.0; //total income
+  let totExpense = 0.0; //total expenses
+  let cellAmount = 0;
+  for (let i = 0, row; (row = transactionHistory.rows[i]); i++) {
+    for (let j = 0, col; (col = row.cells[j]); j++) {
+      if (j === 2) {
+        cellAmount = col.innerHTML; //Save amount
+        // console.log('Each CELL DATA : ' + col.innerHTML);
+      }
+      if (j === 3 && col.innerHTML === 'expense') {
+        runningBal = parseFloat(runningBal) - parseFloat(cellAmount);
+        totExpense = parseFloat(totExpense) + parseFloat(cellAmount);
+      } else if (j === 3 && col.innerHTML === 'income') {
+        runningBal = parseFloat(runningBal) + parseFloat(cellAmount);
+        totIncome = parseFloat(totIncome) + parseFloat(cellAmount);
+      }
+    }
   }
+
+  totalIncome.textContent = totIncome;
+  totalExpenses.textContent = totExpense;
   balance.textContent = runningBal;
 }
+
+// clearAll button
+clearAllBtn.addEventListener('click', () => {
+  while (transactionHistory.rows.length > 0) {
+    transactionHistory.deleteRow(0);
+  }
+  totalExpenses.textContent = 0;
+  totalIncome.textContent = 0;
+  balance.textContent = 0;
+});
